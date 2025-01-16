2e9dc42922aaaaf0263f5573e99def53b6decb24
Mert Adem Özseven <developer@mertozseven.com>
2025-01-16 21:45:44 +0300

    Add: Added initial project

---
+++ Reminder/Reminder/Scene/Home/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Reminder
+//
+//  Created by Mert Ozseven on 16.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // MARK: - Properties
+
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureView()
+    }
+
+    // MARK: - Private Methods
+    private func configureView() {
+        addViews()
+        configureLayout()
+    }
+
+    private func addViews() {
+
+    }
+
+    private func configureLayout() {
+
+    }
+
+}
+

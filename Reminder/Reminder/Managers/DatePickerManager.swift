a099899197922d46f46fb9d31581f5694466a0b3
Mert Adem Özseven <developer@mertozseven.com>
2025-01-24 12:52:29 +0300

    Edit: DatePickerManager documentation.

--- Reminder/Reminder/Managers/DatePickerManager.swift
+++ Reminder/Reminder/Managers/DatePickerManager.swift
@@ -19,28 +19,30 @@ final class DatePickerManager {
 
 extension DatePickerManager {
     /// Presents a date picker view controller.
+    ///
     /// - Parameters:
-    ///  - viewController: The view controller to present the date picker.
-    ///  - sourceView: The view to anchor the popover.
-    ///  - initialDate: The initial date to display in the picker.
-    ///  - pickerMode: The mode of the date picker.
-    ///  - pickerStyle: The style of the date picker.
-    ///  - popoverSize: The size of the popover.
-    ///  - onDateSelected: The closure to be called when a date is selected
-    ///  - selectedDate: The selected date.
-    ///  - Example:
-    ///  ```
-    ///  DatePickerManager.shared.presentDatePicker(
+    ///   - viewController: The view controller to present the date picker.
+    ///   - sourceView: The view to anchor the popover.
+    ///   - initialDate: The initial date to display in the picker.
+    ///   - pickerMode: The mode of the date picker (`.date` or `.time`).
+    ///   - pickerStyle: The style of the date picker (e.g., `.inline`, `.wheels`).
+    ///   - popoverSize: The size of the popover. Default is `CGSize(width: 320, height: 320)`.
+    ///   - onDateSelected: The closure called when a date is selected, passing the selected date as a parameter.
+    ///
+    /// ## Usage
+    /// Here's an example of how to use the `DatePickerManager`:
+    /// ```swift
+    /// DatePickerManager.shared.presentDatePicker(
     ///     from: self,
     ///     sourceView: view,
     ///     initialDate: Date(),
     ///     pickerMode: .date,
     ///     pickerStyle: .inline,
     ///     onDateSelected: { date in
-    ///     print(date)
+    ///         print(date)
     ///     }
-    ///  )
-    ///  ```
+    /// )
+    /// ```
     func presentDatePicker(
         from viewController: UIViewController,
         sourceView: UIView,

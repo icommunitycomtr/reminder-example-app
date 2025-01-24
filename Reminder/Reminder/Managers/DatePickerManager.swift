ff6f3535fdb5a44a88fd05d050d8ce0479e8b830
Mert Adem Özseven <developer@mertozseven.com>
2025-01-24 12:38:06 +0300

    Add: DatePickerManager documentation.

--- Reminder/Reminder/Managers/DatePickerManager.swift
+++ Reminder/Reminder/Managers/DatePickerManager.swift
@@ -12,11 +12,35 @@ final class DatePickerManager {
     // MARK: Singleton
 
     static let shared = DatePickerManager()
-
     private init() {}
+}
 
-    // MARK: Methods
+// MARK: - Public Methods
 
+extension DatePickerManager {
+    /// Presents a date picker view controller.
+    /// - Parameters:
+    ///  - viewController: The view controller to present the date picker.
+    ///  - sourceView: The view to anchor the popover.
+    ///  - initialDate: The initial date to display in the picker.
+    ///  - pickerMode: The mode of the date picker.
+    ///  - pickerStyle: The style of the date picker.
+    ///  - popoverSize: The size of the popover.
+    ///  - onDateSelected: The closure to be called when a date is selected
+    ///  - selectedDate: The selected date.
+    ///  - Example:
+    ///  ```
+    ///  DatePickerManager.shared.presentDatePicker(
+    ///     from: self,
+    ///     sourceView: view,
+    ///     initialDate: Date(),
+    ///     pickerMode: .date,
+    ///     pickerStyle: .inline,
+    ///     onDateSelected: { date in
+    ///     print(date)
+    ///     }
+    ///  )
+    ///  ```
     func presentDatePicker(
         from viewController: UIViewController,
         sourceView: UIView,
